0c04016541404cf07ae702a5b4dace153e7be50a
Pankaj Yogi <pankaj.yogi@outlook.com>
2021-06-20 14:21:57 +0530

    adds maven project files from start.spring.io

---
+++ src/main/java/com/github/pankajyogi/jdbcrest/JdbcRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.pankajyogi.jdbcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JdbcRestApplication.class, args);
+  }
+
+}
